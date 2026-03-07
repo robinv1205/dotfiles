5c52fa0bcb9ac6f35e26a0a73e6dae113c87196c
robinv1205 <robin.vogl1205@icloud.com>
2026-03-07 14:45:34 +0100

    added nvim

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,18 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- Zeilennummern
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Suche
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Kein Swapfile
+vim.opt.swapfile = false
