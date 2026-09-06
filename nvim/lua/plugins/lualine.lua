4a5a777c6657324c39b3dc939801a260e0b01db1
z3r0-del <robin.vogl1205@icloud.com>
2026-09-06 20:59:03 +0200

    updated nvim config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,13 +1,58 @@
--- Lualine onfig
+-- ~/.config/nvim/lua/plugins/lualine.lua
 return {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
-        opts.sections.lualine_z = {
-            function()
-                return os.date("%a %I:%M %p")
-            end,
-            "progress",
-            "location",
+        -- Theme
+        opts.options.theme = "powerline_dark"
+
+        -- Separators
+        opts.options.component_separators = { left = "", right = "" }
+        opts.options.section_separators = { left = "", right = "" }
+
+        -- LSP-Name Component
+        local lsp_name = function()
+            local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+            if #clients == 0 then
+                return "No LSP"
+            end
+            local names = {}
+            for _, c in ipairs(clients) do
+                table.insert(names, c.name)
+            end
+            return table.concat(names, ", ")
+        end
+
+        -- Sections
+        opts.sections = {
+            lualine_a = {
+                {
+                    "mode",
+                    fmt = function(str)
+                        return "-- " .. str .. " --"
+                    end,
+                },
+            },
+            lualine_b = {
+                {
+                    "branch",
+                    icons_enabled = true,
+                    icon = "",
+                },
+                "diff",
+                "diagnostics",
+            },
+            lualine_c = {
+                "filename",
+            },
+            lualine_x = {
+                lsp_name,
+                "filetype",
+            },
+            lualine_z = {
+                function()
+                    return os.date("%a %I:%M %p")
+                end,
+            },
         }
     end,
 }
